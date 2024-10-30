004621a0faa25dcd1e6e41dd581652752562721d
cristianqsanchez <cristianqsdev@gmail.com>
2024-10-30 04:37:49 -0500

    feat: add users endpoints

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return await this.prismaService.user.create({
+      data: createUserDto,
+    });
+  }
+
+  async findOne(username: string) {
+    return await this.prismaService.user.findFirst({
+      where: { username },
+    });
+  }
+}
